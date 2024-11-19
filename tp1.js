aa0e185e6e48252748a700546fa45d1b260edaf1
meryem boubghy <meryemboubghy02@gmail.com>
2024-11-19 15:11:26 +0100

    exercice 1

---
+++ tp1.js
@@ -0,0 +1,13 @@
+//Exercice 1 :
+let Nombers = ["1", "2", "3", "4", "5"];
+console.log("les Nombres :", Nombers);
+//Exercice 2 :
+let fruits = ["apple", "banana", "cherry"];
+//Affichage de 2eme element :
+console.log("le deuxiéme element du tableau est :", fruits[1]);
+// Exercice 3 :
+//Modifier un element d'un tableau:
+let nbr = [10, 20, 30, 40];
+console.log("les nombres est :", nbr);
+//Modificaton :
+nb;
