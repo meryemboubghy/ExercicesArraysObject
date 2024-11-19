b4021fbfa6229fc72406eaa55b57bf7fc1b5177e
meryem boubghy <meryemboubghy02@gmail.com>
2024-11-19 15:34:23 +0100

    changement de nom

--- ExerciceFacile.js
+++ ExerciceFacile.js
@@ -3,7 +3,7 @@ let Nombers = ["1", "2", "3", "4", "5"];
 console.log("les Nombres :", Nombers);
 //Exercice 2 :
 let fruits = ["apple", "banana", "cherry"];
-//Affichage de 2eme element :
+//Affichage le 2eme element :
 console.log("le deuxiéme element du tableau est :", fruits[1]);
 // Exercice 3 :
 //Modifier un element d'un tableau:
@@ -12,3 +12,4 @@ console.log("les nombres est :", nbr);
 //Modificaton :
 nbr[3] = 50;
 console.log("le nouveau table", nbr);
+//
