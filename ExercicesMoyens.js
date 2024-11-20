9abb8a6c3eb3888837f2096b587831a06a26382d
meryem boubghy <meryemboubghy02@gmail.com>
2024-11-20 21:33:20 +0100

    Exercic 12

--- ExercicesMoyens.js
+++ ExercicesMoyens.js
@@ -4,3 +4,7 @@ let numbers = [1, 2, 3, 4, 5];
 for (let number of numbers) {
   console.log(number);
 }
+// Exercice 12==>Filtrer les éléments d'un tableau
+let nombers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let nbrPairs = nombers.filter((n) => n % 2 === 0);
+console.log("Les Nombres Pairs est :" + nbrPairs);
