f1e92e3d5c9b9639b72f2443d60890534fbe185c
meryem boubghy <meryemboubghy02@gmail.com>
2024-11-20 21:13:29 +0100

    Exercice 11

---
+++ ExercicesMoyens.js
@@ -0,0 +1,6 @@
+// Exercice 11
+console.log("Boucles Sur les éléments d'un tableau");
+let numbers = [1, 2, 3, 4, 5];
+for (let number of numbers) {
+  console.log(number);
+}
--- tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-console.log("Avec Methode Simple :");
